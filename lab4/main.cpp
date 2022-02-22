b9c7a651e486cac2ecc5e24125cf2482287009e0
Pavel Takunov <pavel.takynov@gmail.com>
2022-02-22 22:06:45 +0300

    morze filter completed

---
+++ lab4/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <string>
+#include <map>
+#include "algo.h"
+
+using namespace std;
+
+int main(){
+    string symbols;
+    //cin >> symbols;
+    getline(cin,symbols); 
+    for(int i = 0; i < symbols.length(); i++){
+        cout << convert(symbols[i]);
+    }
+    cout << endl;
+    return 0;
+}
